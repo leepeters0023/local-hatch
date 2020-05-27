3ad282ef06b70a76fa20aef44381f45f96215b52
Lee Peters <lpeters0023@gmail.com>
2020-05-27 16:58:23 -0400

    javascript fetch

--- scrape.js
+++ scrape.js
@@ -1,10 +1,31 @@
-// trying with JavaScript
-/*
-const fetch = require('isomorphic-fetch')
-
-(async () => {
-  const response = await fetch('https://www.orvis.com/fishing_report.aspx?locationid=5969');
-  const text = await response.text();
-  console.log(text.match(/(?<=\<h1>).*(?=\<\/h1>)/));
-})()cd
+/*async function getCodes() {
+  for (let i = 0; i < 999; i++) {
+   fetch(`https://www.orvis.com/fishing_report.aspx?locationid=${i}`)
+      .then(response => {
+        if (response.ok) {
+          console.log('ok')
+        } else {
+          console.log(response.statusCode)
+        }
+      })
+      .catch(error => console.log('error is', error));
+  }
+}
 */
+getCodes() 
+function getCodes() {
+  for (let i = 0; i < 999; i++) {
+fetch(`https://www.orvis.com/fishing_report.aspx?locationid=${i}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.blob();
+  })
+  .then(myBlob => {
+    myImage.src = URL.createObjectURL(myBlob);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+}}
